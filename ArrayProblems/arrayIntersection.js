58f9eca3c6152472f3a29891b4ce37012e090e49
sakshi-kumari <sakshikumari987@gmail.com>
2022-01-31 23:35:23 +0530

    Intersection of Two Arrays

---
+++ ArrayProblems/arrayIntersection.js
@@ -0,0 +1,28 @@
+Ques::: https://leetcode.com/problems/intersection-of-two-arrays/submissions/
+
+var intersection = function(nums1, nums2) {
+    const arr1 = nums1.length;
+    const arr2 = nums2.length;
+    let arrToIterate, arrToCheck;
+  
+  /* check on which array to iterate */
+    if(arr1 < arr2) {
+        arrToIterate = nums1;
+        arrToCheck = nums2;
+    } else {
+        arrToIterate = nums2;
+        arrToCheck = nums1;
+    }
+
+    let resultArray = [];
+    
+  /* Functionality to push the common element in both the arrays */
+    arrToIterate.forEach( item => {
+        if(arrToCheck.includes(item) && !resultArray.includes(item)) {
+            resultArray.push(item);
+        }
+    });
+    
+    return resultArray;
+    
+};
